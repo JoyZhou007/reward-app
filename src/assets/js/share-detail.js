10840cc7ffb18d6b59559ced1a818a642bec418b
周欢 <zhouhuan@mysteel.com>
2018-07-13 09:56:51 +0800

    fix(share)  share detail

--- src/assets/js/share-detail.js
+++ src/assets/js/share-detail.js
@@ -1,13 +1,24 @@
+const articleId = window.localStorage.getItem('articleId');
+const articleTitle = window.localStorage.getItem('articleTitle');
+const shareData={
+  title: articleTitle, // 分享标题
+  desc: '【我的钢铁】术业有专攻，英雄所见略有不同，说出你的观点，现金红包等着你～', // 分享描述
+  link: "http://192.168.20.72:4200/reward-detail/" + articleId, // 分享链接
+  imgUrl: 'http://m.steelphone.com/share.png', // 分享图标
+  success: function (msg) {
+    //console.log(msg);
+  }
+}
+
+
 if (window.mysteeljs || window.webkit) {
-  const articleId = window.localStorage.getItem('articleId');
-  const articleTitle = window.localStorage.getItem('articleTitle');
 
   const id = "";//分享id
-  const type = "19";//分享类型
-  const title = articleTitle;//分享标题按
-  const desc = "【我的钢铁】术业有专攻，英雄所见略有不同，说出你的观点，现金红包等着你～";//分享描述
-  const link = "http://192.168.20.72:4200/reward-detail/" + articleId;//分享链接
-  const imgUrl = 'http://m.steelphone.com/share.png';//分享图标
+  const type = "";//分享类型
+  const title = shareData.title;//分享标题按
+  const desc = shareData.desc;//分享描述
+  const link = shareData.link;//分享链接
+  const imgUrl = shareData.imgUrl;//分享图标
 
   if (window.mysteeljs) {
     window.mysteeljs && window.mysteeljs.hideShareMenu(false);
@@ -19,3 +30,20 @@ if (window.mysteeljs || window.webkit) {
     window.webkit.messageHandlers.share.postMessage(shareData);
   }
 }
+
+
+window.jssign = function (appId, timestamp, nonceStr, signature) {
+  wx.config({
+    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    appId: appId, // 必填，公众号的唯一标识
+    timestamp: timestamp, // 必填，生成签名的时间戳
+    nonceStr: nonceStr, // 必填，生成签名的随机串
+    signature: signature,// 必填，签名，见附录1
+    jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'hideOptionMenu'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+  });
+}
+wx.ready(function () {
+  wx.onMenuShareAppMessage(shareData);
+  wx.onMenuShareTimeline(shareData);
+  wx.onMenuShareQQ(shareData);
+});
--- src/assets/js/share-home.js
+++ src/assets/js/share-home.js
@@ -1,10 +1,22 @@
+
+const shareData={
+  title: '悬赏问答”获奖名单新鲜出炉，赶快来认领你的现金红包！', // 分享标题
+  desc: '【我的钢铁】每周一个行业热门话题，畅聊不停，评论即有机会赢现金红包，赶紧来参加吧！', // 分享描述
+  link: 'http://192.168.20.72:4200', // 分享链接
+  imgUrl: 'http://m.steelphone.com/share.png', // 分享图标
+  success: function (msg) {
+    //console.log(msg);
+  }
+}
+
 if (window.mysteeljs || window.webkit) {
+
   const id = "";//分享id
-  const type = "19";//分享类型
-  const title = "悬赏问答”获奖名单新鲜出炉，赶快来认领你的现金红包！";//分享标题按
-  const desc = "【我的钢铁】每周一个行业热门话题，畅聊不停，评论即有机会赢现金红包，赶紧来参加吧！";//分享描述
-  const link = "http://192.168.20.72:4200";//分享链接
-  const imgUrl = 'http://m.steelphone.com/share.png';//分享图标
+  const type = "";//分享类型
+  const title = shareData.title;//分享标题按
+  const desc = shareData.desc;//分享描述
+  const link = shareData.link;//分享链接
+  const imgUrl = shareData.imgUrl;//分享图标
 
   if (window.mysteeljs) {
     window.mysteeljs && window.mysteeljs.hideShareMenu(false);
@@ -16,3 +28,20 @@ if (window.mysteeljs || window.webkit) {
     window.webkit.messageHandlers.share.postMessage(shareData);
   }
 }
+
+
+window.jssign = function (appId, timestamp, nonceStr, signature) {
+  wx.config({
+    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    appId: appId, // 必填，公众号的唯一标识
+    timestamp: timestamp, // 必填，生成签名的时间戳
+    nonceStr: nonceStr, // 必填，生成签名的随机串
+    signature: signature,// 必填，签名，见附录1
+    jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'hideOptionMenu'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+  });
+}
+wx.ready(function () {
+  wx.onMenuShareAppMessage(shareData);
+  wx.onMenuShareTimeline(shareData);
+  wx.onMenuShareQQ(shareData);
+});
